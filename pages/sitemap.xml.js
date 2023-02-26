73691cdb6182f94216ba8209f9413ea83b4d7037
blankapadar <blanka.padr@gmail.com>
2023-02-26 23:45:41 +0100

    added sitemap for google indexing

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,39 @@
+const EXTERNAL_DATA_URL = "https://yourphotos/users";
+
+function generateSiteMap(users) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>https://yourphotos.com</loc>
+     </url>
+     ${users
+       .map(({ id }) => {
+         return `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
+       </url>
+     `;
+       })
+       .join("")}
+   </urlset>
+ `;
+}
+
+function SiteMap() {}
+
+export async function getServerSideProps({ res }) {
+  const request = await fetch(EXTERNAL_DATA_URL);
+  const users = await request.json();
+
+  const sitemap = generateSiteMap(users);
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
